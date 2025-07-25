9a9061a653599a83c417a35f094621f5544b8211
LHY <glowayou27@gmail.com>
2025-07-25 10:43:35 +0900

    수정

--- SpringBootBoard/src/main/java/com/smhrd/board/entity/BoardEntity.java
+++ SpringBootBoard/src/main/java/com/smhrd/board/entity/BoardEntity.java
@@ -42,13 +42,14 @@ public class BoardEntity {
 	
 	@Column(nullable=false, updatable=false) // DB에 저장시 insert는 가능하나, update는 불가능
 	private LocalDate writeDay;
-	
+
 	
 	// 글 작성 시 자동으로 writeDay가 입력 되도록 코드 작성
 	// entity가 생성 될 때 실행되는 코드
 	@PrePersist
 	protected void onCreate() {
 		this.writeDay = LocalDate.now();
+		
 	}
 	
 	
